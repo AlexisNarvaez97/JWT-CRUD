8779db6132b602cd3f45c6b039ce715070f15374
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-04-30 17:18:16 -0500

    Añadiendo un drawer menu para navegar entre rutas

---
+++ README.md
@@ -0,0 +1,1 @@
+# JWT | CRUD | IONIC | ANGULAR
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'table',
+    loadChildren: () => import('./pages/table/table.module').then( m => m.TablePageModule)
   },
 ];
 
